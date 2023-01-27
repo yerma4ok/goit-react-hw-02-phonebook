d53a7a3a655b20fda53f898512ad90171bb44c3f
yerma4ok <yerma4ok13@gmail.com>
2023-01-27 23:12:28 +0100

    add component

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+
+export const MainTitle = styled.h1`
+    text-align: center;
+    margin-bottom: 30px;
+    padding-top: 20px;
+    font-size: 40px;
+    letter-spacing: 0.06em;
+    color: #50C878;
+`
+
+export const ContactTitle = styled.h2`
+    text-align: center;
+    margin-top: 20px;
+    margin-bottom: 30px;
+    font-size: 30px;
+    letter-spacing: 0.06em;
+    color: #50C878;
+`
